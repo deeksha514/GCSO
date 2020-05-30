71e183359c0099baa0c7045c0e1de1f13762372a
deeksha514 <65813290+deeksha514@users.noreply.github.com>
2020-05-30 22:11:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
-var car, wall;
+var car,car2,car3, wall;
 var speed,weight
 
 function setup() {
  createCanvas(1600,400);
 
  car= createSprite(50, 200, 30, 30);
+ car2=createSprite(50,125,30,30);
+ car3=createSprite(50,275,30,30);
  wall=createSprite(1500,200,30,height/2);
  wall.shapecolor=color(80,80,80);
 
- speed=random(55,90);
- weight=random(400,1500);
+ speed=random(35,70);
+ speed2=random(50,80);
+ speed3=random(20,40);
 
+ weight=random(800,1200);
+ weight2=random(100,400);
+ weight3=random(400,800);
+  
  car.velocityX=speed;
+ car2.velocityX=speed2;
+ car3.velocityX=speed3;
 
 }
 
@@ -19,7 +28,7 @@ function setup() {
 function draw() {
   background(0,0,0); 
   
-  if(wall.x-car.x<(wall.width+car.width)/2)
+  if(car.collide(wall))
   {
     car.velocityX=0;
 
@@ -42,8 +51,51 @@ function draw() {
 
   }
 
-  wall.depth=car.depth;
-  car.depth=car.depth+1;
+  if(car2.collide(wall))
+  {
+    car2.velocityX=0;
+
+    var deformation=0.5*weight2*speed2*speed2/22500;
+
+    if(deformation>180)
+    {
+      car2.shapeColor=color(255,0,0);
+    }
+
+    if(deformation<180 && deformation>100)
+    {
+      car2.shapeColor=color(230,230,0);
+    }
+
+    if(deformation<100)
+    {
+      car2.shapeColor=color(0,250,0);
+    }
+
+  }
+
+  if(car3.collide(wall))
+  {
+    car3.velocityX=0;
+
+    var deformation=0.5*weight3*speed3*speed3/22500;
+
+    if(deformation>180)
+    {
+      car3.shapeColor=color(255,0,0);
+    }
+
+    if(deformation<180 && deformation>100)
+    {
+      car3.shapeColor=color(230,230,0);
+    }
+
+    if(deformation<100)
+    {
+      car3.shapeColor=color(0,250,0);
+    }
+
+  }
 
   text("car turns green then the deformation is less than 100",80,50);
   text("car turns yellow then the deformation is between 100 and 180",80,75);
