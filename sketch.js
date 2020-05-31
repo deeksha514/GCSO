655c627d3568a18df040a6e1a0657fb05b88fd7a
deeksha514 <65813290+deeksha514@users.noreply.github.com>
2020-05-31 12:23:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ function setup() {
  wall=createSprite(1500,200,30,height/2);
  wall.shapecolor=color(80,80,80);
 
- speed=random(35,70);
- speed2=random(50,80);
- speed3=random(20,40);
+ speed=random(35,80);
+ speed2=random(60,140);
+ speed3=random(90,150);
 
- weight=random(800,1200);
- weight2=random(100,400);
- weight3=random(400,800);
+ weight=random(400,700);
+ weight2=random(500,1200);
+ weight3=random(180,800);
   
  car.velocityX=speed;
  car2.velocityX=speed2;
@@ -34,21 +34,26 @@ function draw() {
     car.velocityX=0;
 
     var deformation=0.5*weight*speed*speed/22500;
- 
 
-    if(deformation>180)
+    if (deformation>200)
     {
       car.shapeColor=color(255,0,0);
-      text("damage"+round(deformation),500,200);
+      text("Damage:"+round(deformation),500,200);
     }
 
-    if(deformation<180 && deformation>100)
+    if(deformation>120 && deformation<200);
+    {
+      car.shapeColor=color(255, 143, 0);
+      text("Damage:"+round(deformation),500,200);
+    }
+
+    if(deformation<120 && deformation>50)
     {
       car.shapeColor=color(230,230,0);
-      text("damage"+round(deformation),500,200);
+      text("Damage;"+round(deformation),500,200);
     }
 
-    if(deformation<100)
+    if(deformation<50)
     {
       car.shapeColor=color(0,250,0);
       text("Damage:"+round(deformation),500,200);
@@ -64,19 +69,25 @@ function draw() {
 
     var deformation=0.5*weight2*speed2*speed2/22500;
 
-    if(deformation>180)
+    if (deformation>200)
     {
       car2.shapeColor=color(255,0,0);
       text("Damage:"+round(deformation),500,125);
     }
 
-    if(deformation<180 && deformation>100)
+    if(deformation>120 && deformation<200)
+    {
+      car2.shapeColor=color(255, 143, 0);
+      text("Damage:"+round(deformation),500,125);
+    }
+
+    if(deformation<120 && deformation>50)
     {
       car2.shapeColor=color(230,230,0);
       text("Damage:"+round(deformation),500,125);
     }
 
-    if(deformation<100)
+    if(deformation<50)
     {
       car2.shapeColor=color(0,250,0);
       text("Damage:"+round(deformation),500,125);
@@ -94,19 +105,25 @@ function draw() {
 
     var deformation=0.5*weight3*speed3*speed3/22500;
 
-    if(deformation>180)
+    if (deformation>200)
     {
       car3.shapeColor=color(255,0,0);
       text("Damage:"+round(deformation),500,275);
     }
 
-    if(deformation<180 && deformation>100)
+    if(deformation>120 && deformation<200)
+    {
+      car3.shapeColor=color(255, 143, 0);
+      text("Damage:"+round(deformation),500,275);
+    }
+
+    if(deformation<120 && deformation>50)
     {
       car3.shapeColor=color(230,230,0);
       text("Damage:"+round(deformation),500,275);
     }
 
-    if(deformation<100)
+    if(deformation<50)
     {
       car3.shapeColor=color(0,250,0);
       text("Damage:"+round(deformation),500,275);
@@ -116,9 +133,10 @@ function draw() {
   car3.depth=wall.depth;
   car3.depth=car3.depth+1;
 
-  text("car turns green then the deformation is less than 100",80,50);
-  text("car turns yellow then the deformation is between 100 and 180",80,75);
-  text("car turns red then the deformation is greater than 180",80,100);
+  text("car turns green then the deformation is less than 50",80,25);
+  text("car turns yellow then the deformation is between 50 and 120",80,50);
+  text("car turns orange then the deformation is between 120 and 200 ",80,75);
+  text("car turns red then the deformaion is greater than 200",80,100);
 
   drawSprites();
 }
